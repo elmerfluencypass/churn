2686f1b272e509f30d9ed69183a897282b208dca
elmerfluencypass <elmer.dotti@fluencypass.com>
2025-04-07 20:50:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from auth import login
 from data_loader import load_all_data
-from plots import show_churn_dashboard
+from plots import show_churn_dashboard, show_churn_profile
 
 st.set_page_config(page_title="Churn Analysis", layout="wide")
 
@@ -10,7 +10,8 @@ def main():
         return
 
     st.image("logo.webp", width=150)
-    menu = st.sidebar.selectbox("Menu", ["Dataviz"])
+
+    menu = st.sidebar.selectbox("Menu", ["Dataviz", "Perfil de Churn"])
 
     if menu == "Dataviz":
         with st.spinner("Carregando dados..."):
@@ -18,6 +19,13 @@ def main():
             data = load_all_data()
             progress.progress(100)
         show_churn_dashboard(data)
+
+    elif menu == "Perfil de Churn":
+        with st.spinner("Carregando dados..."):
+            progress = st.progress(0)
+            data = load_all_data()
+            progress.progress(100)
+        show_churn_profile(data)
 
 if __name__ == "__main__":
     main()
