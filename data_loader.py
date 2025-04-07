405eb5a489dc209f99721ca971d808bf64517f96
elmerfluencypass <elmer.dotti@fluencypass.com>
2025-04-07 18:30:16 -0300

    Create data_loader.py

---
+++ data_loader.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def load_all_data():
+    urls = {
+        "customer_profile_table": "https://drive.google.com/uc?id=1MLYWW5Axp_gGFXGF_mPZsK4vqTTBKDlT",
+        "churn_detectado": "https://drive.google.com/uc?id=1kkdfrCjTjyzjqYfX7C9vDSdfod5HBYQS",
+        "historico_pagamentos": "https://drive.google.com/uc?id=1j2RW-ryt4H3iX8nkaw371yBI0_EGA8MY",
+        "tbl_insider": "https://drive.google.com/uc?id=1tPqnQWmowQKNAx2M_4rLW5axH9DZ5TyW",
+        "iugu_invoices": "https://drive.google.com/uc?id=1eNcobHn8QJKduVRcs79LsbzT_2L7hK88",
+    }
+
+    return {
+        name: pd.read_csv(url) for name, url in urls.items()
+    }
